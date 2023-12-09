31e4123eaab741b0b2685e846a199616c144407c
Gingene <ginx2618@gmail.com>
2023-12-09 15:56:11 +0800

    fix: fixed the router

--- js/all.js
+++ js/all.js
@@ -1,10 +1,19 @@
 import "./helper/axios.min.js";
 const currentPath = window.location.pathname;
 
-if (currentPath === "/EmojiHub.html") {
+if (
+  currentPath === "/EmojiHub.html" ||
+  currentPath === "/2023JS-week9-workAndTodoList/EmojiHub.html"
+) {
   import("./app/emojihub.js");
-} else if (currentPath === "/GiphyHub.html") {
+} else if (
+  currentPath === "/GiphyHub.html" ||
+  currentPath === "/2023JS-week9-workAndTodoList/GiphyHub.html"
+) {
   import("./app/giphyhub.js");
-} else if (currentPath === "/todoList.html") {
+} else if (
+  currentPath === "/todoList.html" ||
+  currentPath === "/2023JS-week9-workAndTodoList/todoList.html"
+) {
   import("./app/todoList.js");
 }
